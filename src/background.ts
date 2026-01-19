9e4a3a1bf80f5a688523a01fc92ae2fa11b6f6b8
Gavin Kondrath <78187175+gavink97@users.noreply.github.com>
2026-01-19 20:44:55 +0930

    ci: added amo link

--- src/background.ts
+++ src/background.ts
@@ -8,9 +8,19 @@ var Slop = new Map<String, Boolean>([
 	["grok.com", true],
 ]);
 
-//var caughtSlipping = 0;
+export async function LoadVar(key: string): Promise<any> {
+	const local_storage = browser.storage.local;
+	const result = await local_storage.get(key);
+	return result[key];
+}
+
+async function storeVar(key: string, value: any) {
+	const local_storage = browser.storage.local;
+	local_storage.set({ [key]: value });
+}
 
 async function main() {
+	let count: number = (await LoadVar("count")) ?? 0;
 	const currentTab = await browser.tabs.query({
 		active: true,
 		currentWindow: true,
@@ -24,15 +34,30 @@ async function main() {
 	const baseUrl = splits[2];
 
 	if (Slop.get(baseUrl) == true) {
-		/*
-		await browser.action.setBadgeText({
-			text: (caughtSlipping++).toString(),
-		});
-		*/
 		await browser.tabs.remove(currentTab[0].id);
+		count += 1;
+		await browser.action.setBadgeText({
+			text: count.toString(),
+		});
+		storeVar("count", count);
+		return;
 	}
 }
 
-browser.tabs.onUpdated.addListener(async () => {
-	main();
+browser.windows.onCreated.addListener(async () => {
+	let count: number = (await LoadVar("count")) ?? 0;
+
+	await browser.action.setBadgeText({
+		text: count.toString(),
+	});
 });
+
+if (
+	!browser.tabs.onUpdated.hasListener(async () => {
+		await main();
+	})
+) {
+	browser.tabs.onUpdated.addListener(async () => {
+		await main();
+	});
+}
